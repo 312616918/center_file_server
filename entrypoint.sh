e4a0ba53dbdbc17c0c5b0dc8b446f104d59b91b9
31261 <312616918@qq.com>
2023-05-23 02:50:46 +0800

    up entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 
-nohop python -m flask run --host=0.0.0.0 &
-nohop python streamlit run streamlit_app.py &
+nohop python -m flask run --host=0.0.0.0 --no-debug &
+nohop python streamlit run streamlit_app.py --server.disableFileWatcher &
